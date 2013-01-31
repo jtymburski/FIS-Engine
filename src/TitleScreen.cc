69467c1205e11e6482107ea2ff3cdfb36ce3fc56
Jordan Tymburski <jordan.tymburski@gmail.com>
2013-01-31 00:25:32 -0700

    Working on title screen. Only playing around with available functions, etc. but didn't want to lose my progress. Top left may be the menu option selection color.

--- src/TitleScreen.cc
+++ src/TitleScreen.cc
@@ -6,6 +6,7 @@
 ******************************************************************************/
 #include "TitleScreen.h"
 
+#include <QLabel>
 
 /* Constructor function */
 TitleScreen::TitleScreen(QWidget* parent)
@@ -14,8 +15,22 @@ TitleScreen::TitleScreen(QWidget* parent)
 
   QPalette pal(palette());
   pal.setColor(QPalette::Background, Qt::black);
+  pal.setColor(QPalette::Foreground, Qt::white);
   setAutoFillBackground(true);
   setPalette(pal);
+
+  QFont current_font = font();
+  current_font.setPixelSize(30);
+  current_font.setWeight(QFont::Bold);
+  setFont(current_font);
+
+  QLabel* label = new QLabel("Hello sunshine...", this);
+  QPalette pal2(label->palette());
+  pal2.setColor(QPalette::Foreground, Qt::white);
+  pal2.setColor(QPalette::Background, QColor(28,76,46,64)); // R,G,B,A
+  label->setPalette(pal2);
+  label->setMargin(5);
+  label->setAutoFillBackground(true);
 
   show();
 }
@@ -30,6 +45,9 @@ void TitleScreen::paintEvent(QPaintEvent* event)
   QPainter painter(this);
 
   painter.drawPixmap(520, 100, QPixmap(":/FBS_Logo"));
+
+  painter.drawText(250, 250, "BATTLE TEST");
+  painter.drawText(250, 300, "MAP TEST");
 }
 
 /* Changes the menu to state s and the given index */
