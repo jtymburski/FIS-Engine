2ed04bc78ad6df0ef0d26f3ae4012217204284bc
Michael Van Ryn <vanryn.michael@gmail.com>
2013-03-02 13:40:22 -0700

    Added BattleOption flags and toggle/set/get functions to Game.

--- src/Game/Game.cc
+++ src/Game/Game.cc
@@ -26,3 +26,37 @@ void Game::setEnabled (bool enable)
 void Game::updateGame()
 {
 }
+
+/*
+ * Description: Evaluates a given BattleOption flag
+ *
+ * Inputs: BattleOption - flag to be evaluated
+ * Output: const bool   - evaluation of the flag
+ */
+const bool Game::getBattleFlag(BattleOption flags)
+{
+  return bo_flag_set.testFlag(flags);
+}
+
+/*
+ * Description: Flips a given BattleOption flag
+ *
+ * Inputs: BattleOption - flag to be flipped
+ * Output: none
+ */
+void Game::toggleBattleFlag(BattleOption flags)
+{
+  setBattleFlag(flags, !getBattleFlag(flags));
+}
+
+/*
+ * Description: Sets ta given BattleOption flag
+ *
+ * Inputs: BattleOption - flag to be set
+ *         const bool   - value to set flag to
+ * Output: none
+ */
+void Game::setBattleFlag(BattleOption flags, const bool set_value)
+{
+  (set_value) ? (bo_flag_set |= flags) : (bo_flag_set ^= flags);
+}
