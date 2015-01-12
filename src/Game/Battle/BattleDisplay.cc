6523be266a35a223cf8f42dc96fb9451435f7223
Jesze Kaszas <forest.bear.01@gmail.com>
2015-01-11 17:36:23 -0700

    Added some more to the raise and lower pleps, also started a text file for how each plep should be rendered once they get hard coded in.

--- src/Game/Battle/BattleDisplay.cc
+++ src/Game/Battle/BattleDisplay.cc
@@ -1861,11 +1861,23 @@ bool BattleDisplay::setBattle(Battle* battle, SDL_Renderer* renderer)
 
     /* TODO: Temporary - battle testing */
     temp_sprite = new Sprite(system_options->getBasePath() + 
-                    "sprites/Battle/Pleps/scaldplep_AA_A", 5, ".png", renderer);
-    for(uint16_t i = 0; i < 15; i++)
+                    "sprites/Battle/Pleps/paralysisplep_AA_A", 3, ".png", renderer);
+	temp_sprite->insertTail(system_options->getBasePath() + 
+                              "sprites/Battle/Pleps/paralysisplep_AA_A01.png", renderer);
+	temp_sprite->insertTail(system_options->getBasePath() + 
+                              "sprites/Battle/Pleps/paralysisplep_AA_A00.png", renderer);
+	temp_sprite->insertTail(system_options->getBasePath() + 
+                              "sprites/Battle/Pleps/paralysisplep_AA_A01.png", renderer);
+	temp_sprite->insertTail(system_options->getBasePath() + 
+                              "sprites/Battle/Pleps/paralysisplep_AA_A02.png", renderer);
+	temp_sprite->insertTail(system_options->getBasePath() + 
+                              "sprites/Battle/Pleps/paralysisplep_AA_A01.png", renderer);
+	temp_sprite->insertTail(system_options->getBasePath() + 
+                              "sprites/Battle/Pleps/paralysisplep_AA_A00.png", renderer);
+    for(uint16_t i = 0; i < 5; i++)
       temp_sprite->insertTail(system_options->getBasePath() + 
                               "sprites/blank.png", renderer);
-    temp_sprite->setAnimationTime(90);
+    temp_sprite->setAnimationTime(50);
 
     /* Start the battle */
     success &= startBattle(renderer);
