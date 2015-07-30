d90c6d722e4a9b9fa4ef0ba43236f5453c85c18e
Jordan Tymburski <jordan@forestbear.net>
2015-07-30 00:30:42 -0600

    Fixed bug with seg fault of blockplace in Editor. Started work on NPC bug with random movement...very weird one.

--- src/Game/Map/MapNPC.cc
+++ src/Game/Map/MapNPC.cc
@@ -915,7 +915,10 @@ void MapNPC::update(int cycle_time, std::vector<std::vector<Tile*>> tile_set)
          node_state == RANDOM || node_state == RANDOMRANGE))
       {
         if(node_current->delay > npc_delay)
+        {
           npc_delay += cycle_time;
+          //clearAllMovement(); // Fixes one bug but causes heap of others
+        }
         else
         {
           if(node_state == LOOPED)
