b5e4488b44f3838dfbfd6e015f759d82b1da111e
Jordan Tymburski <jordan@forestbear.net>
2015-10-24 17:41:26 -0600

    Moved all new files from 'project' to 'Project' > unix issues.

---
+++ include/Game/Battle/BattleItem.h
@@ -0,0 +1,30 @@
+#ifndef BATTLE_ITEM_H
+#define BATTLE_ITEM_H
+
+class BattleActor;
+
+#include "EnumDb.h"
+#include "Game/Battle/BattleActor.h"
+#include "Game/Player/Item.h"
+
+/* Structure representing an item and its amt and possible targets */
+class BattleItem
+{
+public:
+  /* Construct a BattleItem object */
+  BattleItem();
+
+  /* The validity of this BattleItem choice */
+  ValidStatus valid_status;
+
+  /* Pointer to the item which would be chosen */
+  Item* item;
+
+  /* The # of items of this type available */
+  uint32_t amount;
+
+  /* The valid targets for the valid item */
+  std::vector<BattleActor*> targets;
+};
+
+#endif
---
+++ include/Game/Battle/BattleSkill.h
@@ -0,0 +1,30 @@
+#ifndef BATTLE_SKILL_H
+#define BATTLE_SKILL_H
+
+class BattleActor;
+
+#include "EnumDb.h"
+#include "Game/Battle/BattleActor.h"
+#include "Game/Player/Item.h"
+
+/* Structure representing a skill and its possible targets for targ. selec. */
+class BattleSkill
+{
+public:
+  /* Construct a BattleSkill object */
+  BattleSkill();
+
+  /* The validity of this BattleSkill choice */
+  ValidStatus valid_status;
+
+  /* The pointer to the skill for this BattleSkill*/
+  Skill* skill;
+
+  /* The true cost of the BattleSkill */
+  uint32_t true_cost;
+
+  /* The valid targets for the Battle Skill */
+  std::vector<BattleActor*> targets;
+};
+
+#endif
