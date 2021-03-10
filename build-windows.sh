83491224fc4675208bd53914a8e24500de06646a
Jordan Tymburski <hello@jordantymburski.com>
2021-03-09 22:39:43 -0800

    Update windows docker build script for MinTTY
    The current bash script is tooled for linux-like compatible
    terminal environments. In Windows, a good option is MinTTY.
    This has an issue with the way it renders windows path to
    find the local directory using `pwd`. It is solved by adding
    a prefix '/'.

--- build-windows.sh
+++ build-windows.sh
@@ -7,5 +7,5 @@
 # Executable can be found in the bin/ directory
 # ==============================================================================
 
-docker run -v $(pwd):/mnt/BuildProject --rm jtymburski/sdl2-cross-make \
+docker run -v /$(pwd):/mnt/BuildProject --rm jtymburski/sdl2-cross-make \
     sh -c 'cd /mnt/BuildProject && make -j4 windows'
